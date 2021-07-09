9d7bdf59b9113617a21be090fc40b2284e7064c9
angelo1104 <madhav1004@icloud.com>
2021-07-09 13:47:32 +0000

    initialized frontned

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,34 @@
+// import "./App.css";
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">hello jan afk</Route>
+        <Route path="/">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
